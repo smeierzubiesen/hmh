fdba56adf76a3cc235ad3c473b8d867a8b510701
Sebastian Meier zu Biesen <mitoskalandiel@gmail.com>
2016-01-07 12:53:43 +0000

    Linux cpp created for testing fun

---
+++ code/linux_handmade.cpp
@@ -0,0 +1,115 @@
+/*
+ // TODO(smzb): Add a better comment template for new .cpp and .h files
+ */
+
+#include <stdio.h>
+
+LRESULT CALLBACK MainWindowCallBack(
+    HWND   Window,
+    UINT   Message,
+    WPARAM WParam,
+    LPARAM LParam)
+{
+    LRESULT Result = 0;
+    switch(Message)
+    {
+        case WM_SIZE:
+        {
+            OutputDebugStringA("WM_SIZE\n");
+        } break;
+        case WM_DESTROY:
+        {
+            OutputDebugStringA("WM_DESTROY\n");
+        } break;
+        case WM_CLOSE:
+        {
+            OutputDebugStringA("WM_CLOSE\n"); // TODO(smzb): This message gets called when the window is getting closed.
+        } break;
+        case WM_ACTIVATEAPP:
+        {
+            OutputDebugStringA("WM_ACTIVATEAPP\n");
+        } break;
+        case WM_PAINT:
+        {
+            //OutputDebugStringA("WM_PAINT"); // NOTE(smzb): This won't be needed anymore
+            PAINTSTRUCT Paint;
+            HDC DeviceContext = BeginPaint(Window, &Paint);
+            int X = Paint.rcPaint.left;
+			int Y = Paint.rcPaint.top;
+			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+            static DWORD Operation = WHITENESS;
+			PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+            if(Operation == WHITENESS)
+            {
+                Operation = BLACKNESS;
+            } else {
+                Operation = WHITENESS;
+            }
+            EndPaint(Window, &Paint);
+        } break;
+        default:
+        {
+            OutputDebugStringA("no message\n");
+            Result = DefWindowProc(Window, Message, WParam, LParam);
+        } break;
+    }
+    return(Result);
+}
+
+int CALLBACK
+Main(
+	HINSTANCE Instance,
+	HINSTANCE PrevInstance,
+	LPSTR     CommandLine,
+	int       ShowCode)
+{
+    WNDCLASS WindowClass = {};
+
+    // TODO(smzb): Check wether HREDRAW and VREDRAW are still necessary
+    WindowClass.style = CS_OWNDC|CS_HREDRAW|CS_VREDRAW;
+    WindowClass.lpfnWndProc = MainWindowCallBack;
+    WindowClass.hInstance = Instance;
+//    WindowClass.hIcon; // TODO(smzb): Make an icon and stick it in here.
+    WindowClass.lpszClassName = "HandmadeHeroWindowClass";
+
+    if(RegisterClass(&WindowClass))
+    {
+        HWND WindowHandle = CreateWindowEx(0,
+                                           WindowClass.lpszClassName,
+                                           "Handmade Hero", // TODO(smzb): maybe some versioning here?
+                                           WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+                                           CW_USEDEFAULT,
+                                           CW_USEDEFAULT,
+                                           CW_USEDEFAULT,
+                                           CW_USEDEFAULT,
+                                           0,
+                                           0,
+                                           Instance,
+                                           0);
+        if (WindowHandle)
+        {
+            for(;;)
+            {
+                MSG Message;
+                BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+                if (MessageResult > 0)
+                {
+                    TranslateMessage(&Message);
+                    DispatchMessage(&Message);
+                } else {
+                    break;
+                }
+            }
+        } else {
+            // TODO(smzb): Log this event !WindowHandle
+        }
+    }
+    else
+    {
+        // TODO(smzb): Log this event !RegisterClass
+    }
+    
+    return(0);
+}
+
