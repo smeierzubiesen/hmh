f9f4fd53bb5f359781d640165af848948b4e4dd5
Sebastian Meier zu Biesen <smzb@mitos-kalandiel.me>
2017-02-01 09:40:33 +0000

    day2 is done and we have created an actual window in which we either set white or black nothing spectacular yet but it's a good start

--- win32_handmade/win32_handmade.cpp
+++ win32_handmade/win32_handmade.cpp
@@ -12,7 +12,105 @@ $Notice: (C) Copyright 2000-2016 by Joker Solutions, All Rights Reserved. $
 #include "win32_handmade.h"
 #include <Windows.h>
 
-int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-	MessageBox(0, "This is Handmade Hero", "Handmade Hero v0.1", MB_OK | MB_ICONINFORMATION);
-	return 1;
+LRESULT CALLBACK MainWindowCallback(HWND Window, UINT Message, WPARAM WParam, LPARAM LParam)
+{
+	LRESULT Result = 0;
+	switch (Message) {
+		case WM_SIZE:
+		{
+			OutputDebugStringA("WM_SIZE\n");
+		} break;
+		case WM_DESTROY:
+		{
+			OutputDebugStringA("WM_DESTROY\n");
+		} break;
+		case WM_CLOSE:
+		{
+			OutputDebugStringA("WM_CLOSE\n");
+		} break;
+		case WM_ACTIVATEAPP:
+		{
+			OutputDebugStringA("WM_ACTIVATEAPP\n");
+		} break;
+		case WM_PAINT:
+		{
+			PAINTSTRUCT Paint;
+			HDC DeviceContext = BeginPaint(Window, &Paint);
+			int X = Paint.rcPaint.left;
+			int Y = Paint.rcPaint.top;
+			int Width = Paint.rcPaint.right - Paint.rcPaint.left;
+			int Height = Paint.rcPaint.bottom - Paint.rcPaint.top;
+			static DWORD Operation = WHITENESS;
+			PatBlt(DeviceContext, X, Y, Width, Height, Operation);
+			if (Operation == WHITENESS) {
+				Operation = BLACKNESS;
+			}
+			else {
+				Operation = WHITENESS;
+			}
+
+			EndPaint(Window,&Paint);
+			OutputDebugStringA("WM_PAINT\n");
+		}
+		default:
+		{
+			Result = DefWindowProc(Window,Message,WParam,LParam);
+			//6OutputDebugStringA("default\n");
+		} break;
+	}
+	
+	return(Result);
+}
+
+int CALLBACK WinMain(HINSTANCE Instance, HINSTANCE hPrevInstance, LPSTR CommandLine, int ShowCode) {
+	WNDCLASS WindowClass = {};
+	WindowClass.style = CS_HREDRAW|CS_VREDRAW|CS_OWNDC;
+	WindowClass.lpfnWndProc = MainWindowCallback;
+	WindowClass.hInstance = Instance;
+	//WindowClass.hIcon = ;
+	//WindowClass.hCursor = ;
+	//WindowClass.hbrBackground = ;
+	//WindowClass.lpszMenuName = ;
+	WindowClass.lpszClassName = "hmhWindowClass";
+	if (RegisterClass(&WindowClass))
+	{
+		HWND WindowHandle = CreateWindowEx(
+			0,
+			WindowClass.lpszClassName,
+			"Handmade Hero v0.1",
+			WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			CW_USEDEFAULT,
+			0,
+			0,
+			Instance,
+			0);
+		if (WindowHandle)
+		{
+			for (;;)
+			{
+				MSG Message;
+				BOOL MessageResult = GetMessage(&Message, 0, 0, 0);
+				if (MessageResult > 0) {
+					TranslateMessage(&Message);
+					DispatchMessage(&Message);
+				}
+				else {
+					break;
+				}
+			}
+			
+		}
+		else
+		{
+			//TODO(smzb): Log this event
+		}
+	}
+	else {
+		//TODO(smzb): log this event
+	}
+	//MessageBox(0, "This is Handmade Hero", "Handmade Hero v0.1", MB_OK | MB_ICONINFORMATION);
+	return 0;
 }
