4fd45e85b3c4f123ba3a286e16ce51a61025cbfc
Sebastian Meier zu Biesen <smzb@mitos-kalandiel.me>
2017-02-06 14:33:57 +0000

    XInput support is nearly done, we now have empty functions in case there is no controller plugged in/no dll loaded.

--- win32_handmade/win32_handmade.cpp
+++ win32_handmade/win32_handmade.cpp
@@ -14,6 +14,24 @@ $Notice: (C) Copyright 2000-2016 by Joker Solutions, All Rights Reserved. $
 #include <Xinput.h>
 #include "win32_handmade.h"
 
+// NOTE(smzb): XInputGetState
+#define X_INPUT_GET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_STATE *pState)
+typedef X_INPUT_GET_STATE(x_input_get_state);
+X_INPUT_GET_STATE(XInputGetStateStub) {
+	return(0);
+}
+global_variable x_input_get_state *XInputGetState_ = XInputGetStateStub;
+#define XInputGetState XInputGetState_
+
+// NOTE(smzb): XInputSetState
+#define X_INPUT_SET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_VIBRATION *pVibration)
+typedef X_INPUT_SET_STATE(x_input_set_state);
+X_INPUT_SET_STATE(XInputSetStateStub) {
+	return(0);
+}
+global_variable x_input_set_state *XInputSetState_ = XInputSetStateStub;
+#define XInputSetState XInputSetState_
+
 win32_window_dimensions Win32GetWindowDimensions(HWND WindowHandle) {
 	win32_window_dimensions Result;
 	RECT WindowRect;
