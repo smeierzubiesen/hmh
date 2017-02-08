0b360068bf6d3b5937d378892f384d749d853d25
Sebastian Meier zu Biesen <smzb@mitos-kalandiel.me>
2017-02-08 16:35:52 +0000

    changed the return of the controller stub functions. we are now returning proper win error codes.

--- win32_handmade/win32_handmade.h
+++ win32_handmade/win32_handmade.h
@@ -5,7 +5,7 @@
    $Creator: Sebastian Meier zu Biesen $
    $Notice: (C) Copyright 2000-2016 by Joker Solutions, All Rights Reserved. $
    ======================================================================== */
-
+#include <Xinput.h>
 #if !defined(WIN32_HANDMADE_H)
 #define WIN32_HANDMADE_H
 
@@ -46,7 +46,7 @@ global_variable win32_offscreen_buffer GlobalBackBuffer;
 #define X_INPUT_GET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_STATE *pState)
 typedef X_INPUT_GET_STATE(x_input_get_state);
 X_INPUT_GET_STATE(XInputGetStateStub) {
-	return(0);
+	return(ERROR_DEVICE_NOT_CONNECTED);
 }
 global_variable x_input_get_state *XInputGetState_ = XInputGetStateStub;
 #define XInputGetState XInputGetState_
@@ -55,7 +55,7 @@ global_variable x_input_get_state *XInputGetState_ = XInputGetStateStub;
 #define X_INPUT_SET_STATE(name) DWORD WINAPI name(DWORD dwUserIndex, XINPUT_VIBRATION *pVibration)
 typedef X_INPUT_SET_STATE(x_input_set_state);
 X_INPUT_SET_STATE(XInputSetStateStub) {
-	return(0);
+	return(ERROR_DEVICE_NOT_CONNECTED);
 }
 global_variable x_input_set_state *XInputSetState_ = XInputSetStateStub;
 #define XInputSetState XInputSetState_
