0b15d5a03f7a971c5160202dc91318e3cac70eea
Sebastian Meier zu Biesen <smzb@mitos-kalandiel.me>
2017-02-08 16:41:25 +0000

    bugfix/added robustness on dx load, this should now work on both win7 and win8+ (including win10, confirmed locally).

--- win32_handmade/win32_handmade.cpp
+++ win32_handmade/win32_handmade.cpp
@@ -16,6 +16,9 @@ $Notice: (C) Copyright 2000-2016 by Joker Solutions, All Rights Reserved. $
 
 internal void Win32LoadXInput(void) {
 	HMODULE XInputLibrary = LoadLibraryA("xinput1_4.dll");
+	if (!XInputLibrary) {
+		HMODULE XInputLibrary = LoadLibraryA("xinput1_3.dll");
+	}
 	if (XInputLibrary) {
 		XInputGetState = (x_input_get_state *)GetProcAddress(XInputLibrary, "XInputGetState");
 		XInputSetState = (x_input_set_state *)GetProcAddress(XInputLibrary, "XInputSetState");
