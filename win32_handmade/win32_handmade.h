1c407bba7dc96b3b680379bb51d710f695eb166c
Sebastian Meier zu Biesen <smzb@mitos-kalandiel.me>
2017-02-09 11:03:56 +0000

    disabled debug mode in code.

--- win32_handmade/win32_handmade.h
+++ win32_handmade/win32_handmade.h
@@ -21,7 +21,7 @@
 #define Pi32 3.14159265359f
 
 global_variable bool GlobalRunning;
-global_variable bool Debug = 1;
+global_variable bool Debug = 0;
 
 typedef uint8_t uint8;
 typedef uint16_t uint16;
