1ff7c832631965cd1560f429bc6f1299c87ed2f4
Sebastian Meier zu Biesen <smzb@mitos-kalandiel.me>
2017-06-17 13:41:44 +0100

    bugfix for 32bit DevEnvironment
    made shell.cmd script more robust to load correct environment depending
    on 'bit'-ness of OS

--- misc/shell.cmd
+++ misc/shell.cmd
@@ -1,5 +1,16 @@
 @Echo off
 subst w: D:\Projects
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+for /f "skip=1 delims=" %%x in ('wmic cpu get addresswidth') do if not defined AddressWidth set AddressWidth=%%x
+
+if %AddressWidth%==64 (
+  @Echo 64 bit OS
+  call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+  goto :continue
+) else (
+  @Echo 32 bit OS
+  call "C:\Program Files\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+  goto :continue
+)
+:continue
 set PATH=w:\hmh\misc;%path%
 w:
