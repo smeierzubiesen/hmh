a3a2512f406cf1451ab3ee001965f85805564561
Sebastian Meier zu Biesen <smzb@mitos-kalandiel.me>
2017-06-17 14:26:27 +0000

    some script changes and attempting to go back to VS2017

--- misc/shell.cmd
+++ misc/shell.cmd
@@ -1,5 +1,9 @@
 @Echo off
+@Echo Creating work environment in w:
 subst w: D:\Projects
+@Echo Initializing Visual Studio environment
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86
+@Echo Setting paths
 set PATH=w:\hmh\misc;%path%
 w:
+@Echo Development environment ready. Type 'quit' to close down the environment
